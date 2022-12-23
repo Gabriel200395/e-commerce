bd5955d85c1e963e33e7f6e7d58577ea65973b52
Gabriel Souza <eugabrielf@gmail.com>
2022-12-23 15:24:34 -0300

    desenvolvendo parte produtos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Banner from "./components/banner";
 import Header from "./components/header";
-
+import Products from "./components/products";
+import BannerProduct from "./components/bannerProduct";
 function App() {
   return (
     <>
       <Header />
       <Banner />
+      <Products />
+      <BannerProduct />
     </>
   );
 }
---
+++ src/components/bannerProduct/index.tsx
@@ -0,0 +1,3 @@
+export default function BannerProduct() {
+  return <div>teste</div>;
+}
