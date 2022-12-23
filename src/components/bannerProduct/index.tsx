0d47ee6dbda1b3b896324ed3ff534ee2edd24210
Gabriel Souza <eugabrielf@gmail.com>
2022-12-23 15:53:50 -0300

    terminando desenvolver  banner

--- src/components/bannerProduct/index.tsx
+++ src/components/bannerProduct/index.tsx
@@ -1,3 +1,17 @@
+import tenis from "../../assets/img/tenis.png"
+import "./styles.css";
+
 export default function BannerProduct() {
-  return <div>teste</div>;
+  return (
+    <div className="banner-product-container">
+      <div className="banner-product-container-text">
+        <h2>Adidas Men Running Sneakers</h2>
+        <p>Performance and design. Taken right to the edge.</p>
+        <span>SHOP NOW</span>
+      </div> 
+      <div className="banner-product-container-img">
+        <img src={tenis} alt={tenis}/>
+      </div>
+    </div>
+  );
 }
